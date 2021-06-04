5d1175b623b88753bf21a2bf7c2655b5dad772d5
Aleksan <48214689+Aharonyan@users.noreply.github.com>
2021-06-04 13:56:25 +0300

    remove_filter( 'the_content', 'wpautop' ) added

--- templates/front-template.php
+++ templates/front-template.php
@@ -1,19 +1,21 @@
 <?php
-if (substr(get_site_url(), 0, 5) === "http:" && getenv("PEERBOARD_ENV") !== 'local') :?>
+if (substr(get_site_url(), 0, 5) === "http:" && getenv("PEERBOARD_ENV") !== 'local') : ?>
     <div id='peerboard-forum' class='disabled'>
         Hello, because we are providing full hosting for our boards - we don't serve it for unsecure protocols, such a HTTP.
-        <br/><br/>
+        <br /><br />
         Consider switching to HTTPS - for most admin panels it's one click action.
-        <br/>
+        <br />
         <b>Then reactivate plugin and thats it.</b>
-        <br/><br/>
+        <br /><br />
         Another option is to connect peerboard as a subdomain for your blog, it can be found in hosting section of your board.
-        <br/><br/>
+        <br /><br />
         If you don't have one yet - you can create it here
-        <br/><br/>
+        <br /><br />
         Will be happy to answer questions dropped to <a href='mailto:integrations@peerboard.com'>integrations@peerboard.com</a>
-        <br/><br/>
+        <br /><br />
     </div>
-<?php else: ?>
+<?php else : ?>
     <div id='peerboard-forum'></div>
-<?php endif; ?>
+<?php endif;
+remove_filter('the_content', 'wpautop')
+?>
