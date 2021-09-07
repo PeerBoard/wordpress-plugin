01f6ff82db9654f30f386a195c720a86e74bf377
Aleksan <48214689+Aharonyan@users.noreply.github.com>
2021-09-07 09:10:13 +0300

    fixed small issue

--- inc/Settings.php
+++ inc/Settings.php
@@ -266,7 +266,7 @@ class Settings
             }
             peerboard_update_post_slug($value['prefix']);
 
-            $success = API::peerboard_post_integration($value['auth_token'], $community_full_slug, peerboard_get_domain());
+            $success = API::peerboard_post_integration($value['auth_token'], peerboard_get_comm_full_slug(), peerboard_get_domain());
 
             if (!$success) {
                 return $old_value;
