1f5abc0ee439af9ee4e0dec3babec3422b2a1fae
Aharonyan <aleksanaharonyan@gmail.com>
2021-10-27 18:12:19 +0500

    added useragents list

---
+++ inc/SSR.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PEBO;
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+/**
+ * Class to sync and work with users
+ */
+class SSR
+{
+
+    public static $peerboard_options;
+
+    public static function init(){
+        
+    }
+}
+
+SSR::init();
